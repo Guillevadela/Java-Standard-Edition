e6944f940c4bf4d62c9fdc2f68dd7e6f1afaf924
Guillermo <Guillermo@MSI.home>
2020-03-05 21:51:11 +0100

    areas ej A y ej B mios

---
+++ src/examen/pruebas/ejercicios/andermios/EjercicioAExamen.java
@@ -0,0 +1,76 @@
+package examen.pruebas.ejercicios.andermios;
+
+import java.util.Scanner;
+
+public class EjercicioAExamen {
+
+	public static void main(String[] args) {
+		// TODO calcular media
+		// TODO altura maxima
+		// TODO altura minima
+		
+		Scanner sc = new Scanner(System.in);
+		double total = 0;
+		int maxima = 300;
+		int minima = 20;
+		System.out.println("Empezamos");
+		int[] alturas = new int[5];
+		
+
+		for (int i = 0; i < alturas.length; i++) {
+
+			System.out.println("Dime una altura en cm:");
+			int altura = Integer.parseInt(sc.nextLine());
+			alturas[i] = altura;
+
+			if (alturas[i] > maxima) {
+				maxima = alturas[i];
+			}
+			if (alturas[i] < minima) {
+				minima = alturas[i];
+			}	
+		} // for
+		
+		double media = total / 5;
+		System.out.println("Media " + media);
+		System.out.println("Maxima " + maxima);
+		System.out.println("Minima " + minima);
+
+		System.out.println("Gracias y adios");
+
+		sc.close();
+		/*
+		//temper
+		int[] temperaturas = new int[7];
+		double total = 0;
+		int maxima = -110;
+		int minima = 100;
+
+		temperaturas[0] = 12;
+		temperaturas[1] = 35;
+		temperaturas[2] = 15;
+		temperaturas[3] = 2;
+		temperaturas[4] = 14;
+		temperaturas[5] = 19;
+		temperaturas[6] = 41;
+
+		for (int i = 0; i < temperaturas.length; i++) {
+			System.out.println(temperaturas[i]);
+			total += temperaturas[i];
+			// comprobar si la temp
+			if (temperaturas[i] > maxima) {
+				maxima = temperaturas[i];
+			}
+			if (temperaturas[i] < minima) {
+				minima = temperaturas[i];
+			}
+		}//for
+
+		double media = total / 7;
+
+		System.out.println("Media " + media);
+		System.out.println("Maxima " + maxima);
+		System.out.println("Maxima " + minima);*/
+	}//main
+
+}//class
---
+++ src/examen/pruebas/ejercicios/andermios/EjercicioBExamen.java
@@ -0,0 +1,67 @@
+package examen.pruebas.ejercicios.andermios;
+
+import java.util.Scanner;
+
+public class EjercicioBExamen {
+	
+	static Scanner sc;
+
+	public static void main(String[] args) {
+		
+		int opcion = 0;
+		sc = new Scanner(System.in);
+		boolean continuar = true;
+
+		do {
+			pintarMenu();
+			opcion = Integer.parseInt(sc.nextLine());
+
+			switch (opcion) {
+			case 1:
+				calcularDNI();
+				break;
+
+			case 2:
+				convertirMetrosApies();
+				break;
+
+			case 3:
+				System.out.println("Hasta pronto");
+				continuar = false;
+				break;
+
+			default:
+				System.out.println("Por favor selecciona una opcion valida");
+				break;
+
+			}// switch
+
+		} while (continuar);
+
+		sc.close();
+
+	}// main
+
+	private static void pintarMenu() {
+		System.out.println("1. Calcular letra DNI");
+		System.out.println("2. Convertir de pies a metros");
+		System.out.println("3. salir");
+		System.out.println("Por favor selecciona una opcion del 1 al 3");
+
+	}
+
+	private static void convertirMetrosApies() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void calcularDNI() {
+		System.out.println("Dime tu dni");
+		String dni = sc.nextLine();
+		System.out.println(dni);
+
+		
+
+	}
+
+}
