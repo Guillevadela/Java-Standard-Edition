c80a5e58218e51fa382d4b6f2a48a99d3ef2b226
Guillevadela <guillevadela@gmail.com>
2020-03-05 09:46:03 +0100

    nuevos ejercicios en .com no es un lenguaje y en prueba examen

---
+++ src/examen/pruebas/ejercicios/andermios/JugandoConString.java
@@ -0,0 +1,10 @@
+package examen.pruebas.ejercicios.andermios;
+
+public class JugandoConString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/punto/com/no/es/un/lenguaje/Ej10_ExpresionesAlgoritmicas.java
@@ -0,0 +1,13 @@
+package punto.com.no.es.un.lenguaje;
+
+public class Ej10_ExpresionesAlgoritmicas {
+
+	public static void main(String[] args) {
+
+		int N = 5;
+
+		System.out.println(2 * N);
+
+	}
+
+}
