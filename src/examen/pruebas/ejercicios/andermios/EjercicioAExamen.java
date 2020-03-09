7f56030476ffc3b00bb50f9ea2f25fdaa5a244e6
Guillermo <Guillermo@MSI.home>
2020-03-09 18:53:55 +0100

    ejercicio examen commit de limpieza

--- src/examen/pruebas/ejercicios/andermios/EjercicioAExamen.java
+++ src/examen/pruebas/ejercicios/andermios/EjercicioAExamen.java
@@ -8,14 +8,13 @@ public class EjercicioAExamen {
 		// TODO calcular media
 		// TODO altura maxima
 		// TODO altura minima
-		
+
 		Scanner sc = new Scanner(System.in);
 		double total = 0;
 		int maxima = 300;
 		int minima = 20;
 		System.out.println("Empezamos");
 		int[] alturas = new int[5];
-		
 
 		for (int i = 0; i < alturas.length; i++) {
 
@@ -28,9 +27,9 @@ public class EjercicioAExamen {
 			}
 			if (alturas[i] < minima) {
 				minima = alturas[i];
-			}	
+			}
 		} // for
-		
+
 		double media = total / 5;
 		System.out.println("Media " + media);
 		System.out.println("Maxima " + maxima);
@@ -40,37 +39,23 @@ public class EjercicioAExamen {
 
 		sc.close();
 		/*
-		//temper
-		int[] temperaturas = new int[7];
-		double total = 0;
-		int maxima = -110;
-		int minima = 100;
+		 * //temper int[] temperaturas = new int[7]; double total = 0; int maxima =
+		 * -110; int minima = 100;
+		 * 
+		 * temperaturas[0] = 12; temperaturas[1] = 35; temperaturas[2] = 15;
+		 * temperaturas[3] = 2; temperaturas[4] = 14; temperaturas[5] = 19;
+		 * temperaturas[6] = 41;
+		 * 
+		 * for (int i = 0; i < temperaturas.length; i++) {
+		 * System.out.println(temperaturas[i]); total += temperaturas[i]; // comprobar
+		 * si la temp if (temperaturas[i] > maxima) { maxima = temperaturas[i]; } if
+		 * (temperaturas[i] < minima) { minima = temperaturas[i]; } }//for
+		 * 
+		 * double media = total / 7;
+		 * 
+		 * System.out.println("Media " + media); System.out.println("Maxima " + maxima);
+		 * System.out.println("Maxima " + minima);
+		 */
+	}// main
 
-		temperaturas[0] = 12;
-		temperaturas[1] = 35;
-		temperaturas[2] = 15;
-		temperaturas[3] = 2;
-		temperaturas[4] = 14;
-		temperaturas[5] = 19;
-		temperaturas[6] = 41;
-
-		for (int i = 0; i < temperaturas.length; i++) {
-			System.out.println(temperaturas[i]);
-			total += temperaturas[i];
-			// comprobar si la temp
-			if (temperaturas[i] > maxima) {
-				maxima = temperaturas[i];
-			}
-			if (temperaturas[i] < minima) {
-				minima = temperaturas[i];
-			}
-		}//for
-
-		double media = total / 7;
-
-		System.out.println("Media " + media);
-		System.out.println("Maxima " + maxima);
-		System.out.println("Maxima " + minima);*/
-	}//main
-
-}//class
+}// class
