a7349da84c9640fc6e7c40db49a3585874ff7502
Guillevadela <guillevadela@gmail.com>
2020-03-12 13:52:47 +0100

    intento de coche con menu opciones

---
+++ src/examen/pruebas/ejercicios/andermios/ConcesionarioConMenu.java
@@ -0,0 +1,23 @@
+package examen.pruebas.ejercicios.andermios;
+
+import java.util.Scanner;
+
+public class ConcesionarioConMenu {
+
+	public static void main(String[] args) {
+
+		int opcion = 0;
+		Scanner sc = new Scanner(System.in);
+		boolean continuar = true;
+
+		// empieza el menu de opciones
+
+		do {
+
+		} while (continuar);
+
+		sc.close();
+
+	}
+
+}
