62e6db48e7c9c0c82313f6c85598e6ddcf416193
Guillermo <Guillermo@MSI.home>
2020-03-14 12:04:37 +0100

    prueba 12 marzo con scanner

--- src/examen/pruebas/ejercicios/andermios/ConcesionarioConMenu.java
+++ src/examen/pruebas/ejercicios/andermios/ConcesionarioConMenu.java
@@ -6,15 +6,78 @@ public class ConcesionarioConMenu {
 
 	public static void main(String[] args) {
 
-		int opcion = 0;
 		Scanner sc = new Scanner(System.in);
-		boolean continuar = true;
 
-		// empieza el menu de opciones
+		// Inicializamos variables
+		String matricula = "";
+		String marca = "";
+		String modelo = "";
+		String color = "";
+		float potencia = 0f;
+		float cilindrada = 0f;
+		String potenciaTeclado = "";
+		String cilindradateclado = "";
+
+		String masCoches = "";
+		boolean continuar = true;
+		int contador = 0;
+
+		// creamos el array
+		Coche[] arrayCoches = new Coche[5];
+
+		System.out.println("Vamos a guardar los datos de tu coche");
+		System.out.println(" ");
+
+		// Rellenamos el array y vamos preguntando si introducimos mas datos
 
 		do {
+			// Pedimos por pantalla las especificaciones
+			System.out.println("Dame la matricula: ");
+			matricula = sc.nextLine();
+			System.out.println("Dame la marca: ");
+			marca = sc.nextLine();
+			System.out.println("Dame el modelo: ");
+			modelo = sc.nextLine();
+			System.out.println("Dame el color: ");
+			color = sc.nextLine();
+			System.out.println("Dame la potencia ");
+			potenciaTeclado = sc.nextLine();
+			potencia = Float.parseFloat(potenciaTeclado);
+			System.out.println("Dame la cilindrada");
+			cilindradateclado = sc.nextLine();
+			cilindrada = Float.parseFloat(cilindradateclado);
 
-		} while (continuar);
+			// creamos el objeto coche y le asignamos valores
+			Coche coche1 = new Coche();
+			coche1.setMatricula(matricula);
+			coche1.setMarca(marca);
+			coche1.setModelo(modelo);
+			coche1.setColor(color);
+			coche1.setPotencia(0);
+			coche1.setCilindrada(cilindrada);
+
+			// guardamos el coche creado en un array
+			arrayCoches[contador] = coche1;
+			contador++;
+
+			// preguntamos si se van a crear mas coches
+			if (contador <= (arrayCoches.length - 1)) {
+				System.out.println(" ");
+				System.out.println("Quieres crear mas coches? escribe 'si' o 'no'");
+				System.out.println(" ");
+				masCoches = sc.nextLine();
+
+				if ("no".equals(masCoches)) {
+					continuar = false;
+				}
+			}
+
+		} while (continuar && contador <= (arrayCoches.length));
+		// mostramos por pantallael array completo
+		for (int i = 0; i < arrayCoches.length; i++) {
+			System.out.println(" ");
+			System.out.println(arrayCoches[i]);
+		}
 
 		sc.close();
 
