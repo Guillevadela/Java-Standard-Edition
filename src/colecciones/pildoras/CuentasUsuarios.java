84f2af9a9844f08413aebc9603432d92d99bd1d6
Guillermo <Guillermo@MSI>
2020-05-07 21:19:39 +0200

    pruebas git

--- src/colecciones/pildoras/CuentasUsuarios.java
+++ src/colecciones/pildoras/CuentasUsuarios.java
@@ -10,6 +10,7 @@ public class CuentasUsuarios {
 		Cliente cl2 = new Cliente("Rafael Nadal", "00002", 250000);
 		Cliente cl3 = new Cliente("Penelope Cruz", "00003", 300000);
 		Cliente cl4 = new Cliente("Julio Iglesias", "00004", 500000);
+		Cliente cl5 = new Cliente("Antonio Banderas", "00001", 200000);
 
 		Set<Cliente> clientesBanco = new HashSet<Cliente>();
 
@@ -17,6 +18,7 @@ public class CuentasUsuarios {
 		clientesBanco.add(cl2);
 		clientesBanco.add(cl3);
 		clientesBanco.add(cl4);
+		clientesBanco.add(cl5);
 
 		for (Cliente cliente : clientesBanco) {
 			System.out.println(cliente.getNombre() + " " + cliente.getN_cuenta() + " " + cliente.getSaldo());
